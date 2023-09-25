586bf53d87053f5b847976905e890dd6c9471bda
Beatriz <111789906+CreatemyGithub19@users.noreply.github.com>
2023-09-25 17:31:46 -0300

    python clase 5 5.1 -5.3
    añado al repositorio la tarea asignada por el Master Scrum

---
+++ Programación IV/clase1/doc_str.py
@@ -0,0 +1,17 @@
+from mi_clase import MiClase
+
+# help(MiClase)
+
+# print(MiClase.__doc__)
+# print(MiClase.__init__.__doc__)
+print(MiClase.mi_metodo)
+print(type(MiClase.mi_metodo))
+
+
+"""
+Podemos crear documentación
+"""
+
+"""
+estas son las dos maneras de documentar
+"""
---
+++ Programación IV/clase1/mi_clase.py
@@ -0,0 +1,26 @@
+
+
+
+    """
+    Este es un ejemplo de documentación
+    de esta clase.
+    Es solo para saber que aquí esta MiClase
+    """
+    def __init__(self):
+        '''
+        Aquí esta el método dunder init
+        Este método es el inicio
+        para esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        """
+        Este es el método creado por mí que recibe parámetros
+        Detectó los parámetros automáticamente
+        :param param1: Este es el parámetro número 1
+        :param param2: Este es el parámetro número 2
+        :return: Este es el valor de un retorno
+        """
+
+
+
